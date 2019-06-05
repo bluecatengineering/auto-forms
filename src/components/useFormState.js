12d974de6b91c80c67a8a3cd726ee8dd0fdc32bc
JC Estibariz <jcestibariz@users.noreply.github.com>
2019-06-05 11:03:12 -0400

    Add submit function to useFormState (#3)

--- src/components/useFormState.js
+++ src/components/useFormState.js
@@ -8,12 +8,14 @@ import FormContext from './FormContext';
 export default () => {
 	const {
 		state: {initialValues, values, errors, extras},
+		submit,
 	} = useContext(FormContext);
 	return {
 		initialValues,
 		values,
 		errors,
 		extras,
+		submit,
 		isChanged: () => !isEqual(initialValues, values),
 		hasErrors: () => Object.values(errors).some(isNotNull),
 	};
